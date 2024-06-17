815a67272962ddf54264ab955adb56f582509359
Vadim Nikolaev <vadn@yandex.ru>
2024-06-17 13:03:32 +0300

    work_with_JSON.py. наконец то получил список уникальных слов в Новостях

--- work_with_JSON.py
+++ work_with_JSON.py
@@ -20,15 +20,25 @@ def read_json(file_path, word_max_len=6, top_words_amt=10):
     # print(type(news_list), len(news_list))
     # print(news_list)
     for i in range(len(news_list)):
-        words_in_news = news_list[i]['description'].split()
-        #print(len(words_in_news))
+        #words_in_news = news_list[i]['description'].split()
+        words_in_news.append(news_list[i]['description'].split())
+        # print(type(words_in_news))
+        # print(len(words_in_news))
         #words_str = row['description'].split()
         #print(words_str, type(words_str)) 
-        uniq_words = sorted(set(words_in_news))
+        #uniq_words = set(words_in_news)
         #print(len(uniq_words))
-        pass
-    print(len(uniq_words[0]))
-    print(len(words_in_news[0]))
+    words_ = []
+    j = 0
+    while j < len(words_in_news):
+        #print(words_in_news[j])
+        for k in words_in_news[j]:
+            #print(k)
+            words_.append(k)
+        j += 1
+    uniq_words = sorted(set(words_))
+    #print(words_, len(words_))
+    #print(uniq_words, len(uniq_words))
 
     # for w in uniq_words:
     #     #print(w)
