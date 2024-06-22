753d9ae716e744ba83f0f74c3a1f033ba210adcd
Vadim Nikolaev <vadn@yandex.ru>
2024-06-22 23:44:43 +0300

    Вресия от 23:43 - 22.06.2024

--- work_with_JSON.py
+++ work_with_JSON.py
@@ -1,4 +1,4 @@
-#Версия от 18:00 - 17.06.2024
+# Версия от 18:00 - 17.06.2024
 
 import json
 from pprint import pprint
@@ -11,56 +11,56 @@ def read_json(file_path, word_max_len=6, top_words_amt=10):
     # Ваш алгоритм
 
     words_in_news = []
-    #news_list = {}
+    # news_list = {}
     count = 0
     with open('newsafr.json', 'r', encoding='utf-8') as f:
         json_data = json.load(f)
-        #pprint(json_data.values())
+        # pprint(json_data.values())
     news_list = json_data['rss']['channel']['items']
 
     for i in range(len(news_list)):
         words_in_news.append(news_list[i]['description'].split())
         # print(type(words_in_news))
         # print(len(words_in_news))
-    #print(len(words_in_news))
+    # print(len(words_in_news))
 
     # Формируем список слов и словарь уникального списка слов
     words_ = []
     dict_words = {}
     j = 0
     while j < len(words_in_news):
-        #print(words_in_news[j])
+        # print(words_in_news[j])
         for k in words_in_news[j]:
-            #print(len(k))
-            #Слова, где больше 6 символов. Проверка условия word_max_len=6
+            # print(len(k))
+            # Слова, где больше 6 символов. Проверка условия word_max_len=6
             if len(k) >= word_max_len:
-                #print(k)
+                # print(k)
                 words_.append(k)
         j += 1
-    #print(words_)
-    #print(len(words_))
+    # print(words_)
+    # print(len(words_))
     uniq_words = sorted(set(words_))
-    print(uniq_words)
-    print(type(uniq_words), len(uniq_words))
+    # print(uniq_words)
+    # print(type(uniq_words), len(uniq_words))
 
-    #Формируем пустой словарь уникальных слов
-    for uniq_words in words_:
-        dict_words.setdefault(uniq_words,1)
-    #print(dict_words)
-    print(words_.count("ноября"))
-    #Считаем кол-во уникальных слов в новостях
+    for z, n in enumerate(uniq_words):
+    # print(z, n)
+        #print(z, n, words_.count(n))
+        dict_words[n] = words_.count(n)
 
-    v = 0
-    l = len(uniq_words)
-    print(type(l))
-    while v < l:
-    #for z, n in enumerate(uniq_words):
-        #print(z, n)
-        #print(v)
-        v += 1
-        #pass
+    {k: v for k, v in sorted(dict_words.items(), key=lambda item: item[1])}
+    print(dict_words)
+
+
+    # Формируем пустой словарь уникальных слов
+    # for uniq_words in words_:
+    #     dict_words.setdefault(uniq_words, 1)
+    # print(dict_words)
+    #print(words_.count("ноября"))
+    # Считаем кол-во уникальных слов в новостях
 
     return
+
 
 read_json('newsafr.json')
 
