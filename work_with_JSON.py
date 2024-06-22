26c3da4dbba8ce63784265e6402cddc56702e168
Vadim Nikolaev <vadn@yandex.ru>
2024-06-22 23:57:28 +0300

    Вресия от 23:57 - 22.06.2024

--- work_with_JSON.py
+++ work_with_JSON.py
@@ -33,7 +33,7 @@ def read_json(file_path, word_max_len=6, top_words_amt=10):
         for k in words_in_news[j]:
             # print(len(k))
             # Слова, где больше 6 символов. Проверка условия word_max_len=6
-            if len(k) >= word_max_len:
+            if len(k) > word_max_len:
                 # print(k)
                 words_.append(k)
         j += 1
@@ -46,10 +46,10 @@ def read_json(file_path, word_max_len=6, top_words_amt=10):
     for z, n in enumerate(uniq_words):
     # print(z, n)
         #print(z, n, words_.count(n))
-        dict_words[n] = words_.count(n)
+        dict_words[words_.count(n)] = n
 
     {k: v for k, v in sorted(dict_words.items(), key=lambda item: item[1])}
-    print(dict_words)
+    pprint(dict_words)
 
 
     # Формируем пустой словарь уникальных слов
