029af98d8c34c408fc5884f74961a63e3bb534a6
Vadim Nikolaev <vadn@yandex.ru>
2024-06-23 23:55:40 +0300

    Вресия от 23:54 - 23.06.2024 - Наконец правильная длинна словарей ! ) Почти финальное решенеи. НО только слова 'африканских' и 'которые' переставлены местами (

--- work_with_JSON.py
+++ work_with_JSON.py
@@ -13,57 +13,38 @@ def read_json(file_path, word_max_len=6, top_words_amt=10):
     words_in_news = []
     with open('newsafr.json', 'r', encoding='utf-8') as f:
         json_data = json.load(f)
-        # pprint(json_data.values())
     news_list = json_data['rss']['channel']['items']
 
     for i in range(len(news_list)):
         words_in_news.append(news_list[i]['description'].split())
-        # print(type(words_in_news))
-        # print(len(words_in_news))
-    print(words_in_news)
 
     # Формируем список общего количества слов
     words_ = []
     dict_words = {}
     j = 0
     while j < len(words_in_news):
-        # print(words_in_news[j])
         for k in words_in_news[j]:
-            # print(len(k))
             # Слова, где больше 6 символов. Проверка условия word_max_len=6
             if len(k) > word_max_len:
-                # print(k)
                 words_.append(k)
         j += 1
-    # print(words_)
-    #print(len(words_))
+
+    #Формируем список уникальных слов
     uniq_words = sorted(set(words_))
-    print(uniq_words.count('туризма'))
-    print(type(uniq_words), len(uniq_words))
 
     #Формируем словарь слов и их количество
     for z, n in enumerate(uniq_words):
-        #print(n)
-        #print(z, n, words_.count(n))
-        #dict_words[words_.count(n)] = n
         dict_words[n] = words_.count(n)
-        print(z, dict_words, len(dict_words))
-        #print(z, type(dict_words[words_.count(n)]))
-    print(len(dict_words))
 
-    marklist = sorted(dict_words.items(), reverse=True)
-    #print(len(marklist))
-    # for v in len(marklist):
-    #     print(marklist[1])
-    # dict_sorted = dict(marklist)
-    print(len(marklist))
-    # print(dict_sorted.values())
+    #Делаем сортировку списка слов по кол-ву упоменаний
+    sorted_list = sorted(dict_words.items(), key=lambda x: x[1], reverse=True)
+    dict_sorted = dict(sorted_list)
+    final_list = list(dict_sorted.keys())[:top_words_amt]
+    #print(list(dict_sorted.keys())[:top_words_amt])
 
+    return final_list
 
-    return
+#read_json('newsafr.json')
 
-
-read_json('newsafr.json')
-
-# if __name__ == '__main__':
-#     print(read_json('newsafr.json'))
+if __name__ == '__main__':
+    print(read_json('newsafr.json'))
